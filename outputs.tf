e72ec7dac0352efa16cc3d9346377ddde6ebbe83
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:11 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "bot_channel_alexas" {
-  description = "All bot_channel_alexa resources"
-  value       = azurerm_bot_channel_alexa.bot_channel_alexas
-}
 output "bot_channel_alexas_bot_name" {
-  description = "List of bot_name values across all bot_channel_alexas"
-  value       = [for k, v in azurerm_bot_channel_alexa.bot_channel_alexas : v.bot_name]
+  description = "Map of bot_name values across all bot_channel_alexas, keyed the same as var.bot_channel_alexas"
+  value       = { for k, v in azurerm_bot_channel_alexa.bot_channel_alexas : k => v.bot_name }
 }
 output "bot_channel_alexas_location" {
-  description = "List of location values across all bot_channel_alexas"
-  value       = [for k, v in azurerm_bot_channel_alexa.bot_channel_alexas : v.location]
+  description = "Map of location values across all bot_channel_alexas, keyed the same as var.bot_channel_alexas"
+  value       = { for k, v in azurerm_bot_channel_alexa.bot_channel_alexas : k => v.location }
 }
 output "bot_channel_alexas_resource_group_name" {
-  description = "List of resource_group_name values across all bot_channel_alexas"
-  value       = [for k, v in azurerm_bot_channel_alexa.bot_channel_alexas : v.resource_group_name]
+  description = "Map of resource_group_name values across all bot_channel_alexas, keyed the same as var.bot_channel_alexas"
+  value       = { for k, v in azurerm_bot_channel_alexa.bot_channel_alexas : k => v.resource_group_name }
 }
 output "bot_channel_alexas_skill_id" {
-  description = "List of skill_id values across all bot_channel_alexas"
-  value       = [for k, v in azurerm_bot_channel_alexa.bot_channel_alexas : v.skill_id]
+  description = "Map of skill_id values across all bot_channel_alexas, keyed the same as var.bot_channel_alexas"
+  value       = { for k, v in azurerm_bot_channel_alexa.bot_channel_alexas : k => v.skill_id }
 }
 
